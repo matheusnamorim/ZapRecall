84cfb2979ebeedd23438aa30e8262e2e680089eb
Matheus Amorim <matheusnavesamorim@gmail.com>
2022-07-20 15:05:24 -0300

    Componentizando Tela inicial

---
+++ src/components/App.js
@@ -0,0 +1,7 @@
+import HomeScreen from "./HomeScreen";
+
+export default function App(){
+    return (
+     <HomeScreen />
+    );
+}
---
+++ src/components/HomeScreen.js
@@ -0,0 +1,11 @@
+export default function HomeScreen(){
+    return (
+        <div className="homeScreen">
+        <img src="assets/logo.png" alt="logo"/>
+        <p>ZapRecall</p>
+        <div className="loginRecall">
+            <p>Iniciar Recall!</p>
+        </div>
+      </div>
+    );
+}
