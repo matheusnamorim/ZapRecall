f0ddb5840a519b4c8edae373aa941df97a35edd1
Matheus Amorim <matheusnavesamorim@gmail.com>
2022-07-25 15:40:55 -0300

    Escolhendo Deck de FlashCard

--- src/components/App.js
+++ src/components/App.js
@@ -7,24 +7,63 @@ import '../assets/css/main.css';
 function App(){
 
     const arrayQuestions = [
-        {question:"O que é JSX?", answers: "Uma extensão de linguagem do JavaScript", state: false},
-        {question:"O React é _ ", answers: "Uma biblioteca JavaScript para construção de interfaces", state: false},
-        {question:"Componentes devem iniciar com _ ", answers: "Letra maiúscula", state: false},
-        {question:"Podemos colocar _ dentro do JSX", answers: "Expressões", state: false},
-        {question:"O ReactDOM nos ajuda _", answers: "Interagindo com a DOM para colocar componentes React na mesma", state: false},
-        {question:"Usamos o npm para _", answers: "Gerenciar os pacotes necessários e suas dependências", state: false},
-        {question:"Usamos props para _", answers: "Passar diferentes informações para componentes", state: false},
-        {question:"Usamos estado (state) para _", answers: "Dizer para o React quais informações quando atualizadas devem renderizar a tela novamente", state: false}
+        {deck1: [
+            {question:"O que é JSX?", answers: "Uma extensão de linguagem do JavaScript", state: false},
+            {question:"O React é _ ", answers: "Uma biblioteca JavaScript para construção de interfaces", state: false},
+            {question:"Componentes devem iniciar com _ ", answers: "Letra maiúscula", state: false},
+            {question:"Podemos colocar _ dentro do JSX", answers: "Expressões", state: false},
+            {question:"O ReactDOM nos ajuda _", answers: "Interagindo com a DOM para colocar componentes React na mesma", state: false},
+            {question:"Usamos o npm para _", answers: "Gerenciar os pacotes necessários e suas dependências", state: false},
+            {question:"Usamos props para _", answers: "Passar diferentes informações para componentes", state: false},
+            {question:"Usamos estado (state) para _", answers: "Dizer para o React quais informações quando atualizadas devem renderizar a tela novamente", state: false}
+        ], type: 'React 1'},
+        {deck2: [
+            {question:"O que é JSX?", answers: "Uma extensão de linguagem do JavaScript", state: false},
+            {question:"O React é _ ", answers: "Uma biblioteca JavaScript para construção de interfaces", state: false},
+            {question:"Componentes devem iniciar com _ ", answers: "Letra maiúscula", state: false},
+            {question:"Podemos colocar _ dentro do JSX", answers: "Expressões", state: false},
+            {question:"O ReactDOM nos ajuda _", answers: "Interagindo com a DOM para colocar componentes React na mesma", state: false},
+            {question:"Usamos o npm para _", answers: "Gerenciar os pacotes necessários e suas dependências", state: false},
+            {question:"Usamos props para _", answers: "Passar diferentes informações para componentes", state: false},
+            {question:"Usamos estado (state) para _", answers: "Dizer para o React quais informações quando atualizadas devem renderizar a tela novamente", state: false},
+            {question:"O que é JSX?", answers: "Uma extensão de linguagem do JavaScript", state: false},
+            {question:"O React é _ ", answers: "Uma biblioteca JavaScript para construção de interfaces", state: false},
+            {question:"Componentes devem iniciar com _ ", answers: "Letra maiúscula", state: false},
+            {question:"Podemos colocar _ dentro do JSX", answers: "Expressões", state: false},
+            {question:"O ReactDOM nos ajuda _", answers: "Interagindo com a DOM para colocar componentes React na mesma", state: false},
+            {question:"Usamos o npm para _", answers: "Gerenciar os pacotes necessários e suas dependências", state: false},
+            {question:"Usamos props para _", answers: "Passar diferentes informações para componentes", state: false},
+            {question:"Usamos estado (state) para _", answers: "Dizer para o React quais informações quando atualizadas devem renderizar a tela novamente", state: false}
+        ], type: 'React 2'},
+        {deck3: [
+            {question:"O que é JSX?", answers: "Uma extensão de linguagem do JavaScript", state: false},
+            {question:"O React é _ ", answers: "Uma biblioteca JavaScript para construção de interfaces", state: false},
+            {question:"Componentes devem iniciar com _ ", answers: "Letra maiúscula", state: false},
+            {question:"Podemos colocar _ dentro do JSX", answers: "Expressões", state: false},
+            {question:"O ReactDOM nos ajuda _", answers: "Interagindo com a DOM para colocar componentes React na mesma", state: false}
+        ], type: 'React 3'},
     ];
-    arrayQuestions.sort(() => Math.random() - 0.5);
+    
     const [screen, setScreen] = React.useState(0);
-    const [inc , setInc] = React.useState(0);
     const [inputValue, setInputValue] = React.useState('');
+    const [inputValueDeck, setInputValueDeck] = React.useState('');
+    const [length, setLength] = React.useState(0);
 
     if(screen === 0){
-        return (<><HomeScreen screen={setScreen} length={arrayQuestions.length} setInputValue={setInputValue} inc={inc} setInc={setInc}/></>);
+        return (<><HomeScreen array={arrayQuestions} length={length} setLength={setLength} screen={setScreen} setInputValue={setInputValue} setInputValueDeck={setInputValueDeck}/></>);
     }else if(screen === 1){
-        return (<><QuestionsScreen screen={setScreen} zap={inputValue} arrayQuestions={arrayQuestions} /></>);
+        if(inputValueDeck === '1'){
+            arrayQuestions[0].deck1.sort(() => Math.random() - 0.5);
+            return (<><QuestionsScreen screen={setScreen} zap={inputValue} arrayQuestions={arrayQuestions[0].deck1} /></>);
+        }
+        if(inputValueDeck === '2'){
+            arrayQuestions[1].deck2.sort(() => Math.random() - 0.5);
+            return (<><QuestionsScreen screen={setScreen} zap={inputValue} arrayQuestions={arrayQuestions[1].deck2} /></>);
+        }
+        if(inputValueDeck === '3'){
+            arrayQuestions[2].deck3.sort(() => Math.random() - 0.5);
+            return (<><QuestionsScreen screen={setScreen} zap={inputValue} arrayQuestions={arrayQuestions[2].deck3} /></>);
+        }       
     }
 }
 {/**/}
--- src/components/HomeScreen/HomeScreen.js
+++ src/components/HomeScreen/HomeScreen.js
@@ -3,16 +3,27 @@ import './style.css';
 import LoginButton from '../LoginButton/LoginButton';
 import img from '../../assets/img/logo.svg';
 
-export default function HomeScreen({screen, length, setInputValue, inc, setInc}){
+export default function HomeScreen({array, screen, setInputValue, setInputValueDeck, length, setLength}){
 
+    
     const [inputValueAux, setInputValueAux] = React.useState('');
-    if(inc === 0) alert('A meta de zaps deve ser um número entre 1 e ' + length);
-    setInc(1);
+    const [deckAux, setDeckAux] = React.useState('');
 
     function checkValue(){
         setInputValue(inputValueAux);
+
+        if(deckAux === '1') setLength(array[0].deck1.length);
+        if(deckAux === '2') setLength(array[1].deck2.length);
+        if(deckAux === '3') setLength(array[2].deck3.length);
+
         if(inputValueAux <= 0 || inputValueAux > length)  return false;
         else return true;
+    }
+
+    function checkDeck(){
+        setInputValueDeck(deckAux);
+        if(deckAux != '') return true; 
+        else return false
     }
 
     return (
@@ -20,9 +31,20 @@ export default function HomeScreen({screen, length, setInputValue, inc, setInc})
         <img src={img}/>
         <p>ZapRecall</p>
 
+        <div className={`${checkDeck() ? '' : 'hidden'}`}>
         <input className={`${checkValue() ? 'inputZap inputValid' : 'inputZap'}`} value={inputValueAux} onChange={(e) => setInputValueAux(e.target.value)} type='number' placeholder='Digite sua meta de zaps...'/>
+        </div>
+
+        <select name='decks' className={`${checkDeck() ? 'hidden' : 'decks'}`} value={deckAux} onChange={(e) => setDeckAux(e.target.value)}>
+            <option value=''>Escolha seu deck</option>
+            <option value='1'>{array[0].type} (Nº flashCards {array[0].deck1.length})</option>
+            <option value='2'>{array[1].type} (Nº flashCards {array[1].deck2.length})</option>
+            <option value='3'>{array[2].type} (Nº flashCards {array[2].deck3.length})</option>
+        </select>
 
         <LoginButton type={checkValue()} screen={screen}/>
+
+
         
       </div>
     );
